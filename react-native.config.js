193ef17cdf3ab84e685e37b437e2f70bcc62ac6e
anh.tran <anh.tran@leapxpert.net>
2024-05-21 14:40:50 +0700

    chore: [LED-000] Update rn

--- react-native.config.js
+++ react-native.config.js
@@ -1,24 +1,3 @@
-const path = require('path')
-const { androidManifestPath } = require('react-native-test-app')
-
-const project = (() => {
-  try {
-    return {
-      android: {
-        sourceDir: path.join('example', 'android'),
-        manifestPath: androidManifestPath(path.join(__dirname, 'example', 'android')),
-      },
-      ios: {
-        sourceDir: path.join('example', 'ios'),
-      },
-    }
-  } catch (e) {
-    console.error('example config not found', e)
-
-    return undefined
-  }
-})()
-
 module.exports = {
   dependencies: {
     // Help rn-cli find and autolink this library
@@ -26,5 +5,8 @@ module.exports = {
       root: __dirname,
     },
   },
-  ...(project ? { project } : undefined),
+  project: {
+    android: {},
+    ios: {},
+  },
 }
